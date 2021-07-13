b68abc2a2c4ff180be1b95fdffabce2f3f6d4b0f
Thilrash-Ameen <thilrashameen@gmail.com>
2021-07-13 15:11:35 +0530

    python-development-course commit

--- practical/practical1.py
+++ practical/practical1.py
@@ -1,2 +1,13 @@
 # Practical 1
 
+# Question
+# We check in a patient named Jhon Smith. He's 20 years old and is a new patient
+
+_p_name = "Jhon Smith"
+_p_age = 20
+is_newPatient = True
+
+_user_input_name = input('What is the patient name? ')
+
+if _user_input_name == "Jhon Smith" :
+    print('Hello ' + _p_name)
---
+++ practical/practical2.py
@@ -0,0 +1,14 @@
+# Age detection
+_birth_year = input('Birth year: ')
+print(type(_birth_year))
+age = 2021 - int(_birth_year)
+print(type(age))
+print(age)
+
+# Question
+# Ask a user thier weight (in pounds), convert it to kilograms and print on the terminal
+
+_weight_in_pounds = input('Enter your wight (in pounds) : ')
+_convert_pounds_to_weight = int(_weight_in_pounds) * 0.45
+print('Your weight in KG : ' + str(_convert_pounds_to_weight))
+
